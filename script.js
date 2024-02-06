bca5240a8a9dc656af5878db2a16b90af20b7494
beqa <beqa.tskhvediani@gmail.com>
2024-02-06 18:56:53 +0400

    add variables

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const numbers = Array.from(document.querySelectorAll(".number"));
 const display = document.querySelector(".display");
+const numbers = Array.from(document.querySelectorAll(".number"));
+const symbols = Array.from(document.querySelectorAll(".symbol"));
 const deleteBtn = document.querySelector(".delete");
 const reset = document.querySelector(".reset");
+const equals = document.querySelector(".equals");
 
 let displayInput = [];
 let inputValue = 0;
@@ -17,16 +19,27 @@ numbers.forEach((item) => {
   item.addEventListener("click", (event) => {
     buttonValue = Number(event.target.innerText);
     displayInput.push(buttonValue);
-    console.log(displayInput);
+    // console.log(displayInput);
+  });
+});
+symbols.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    buttonValue = Number(event.target.innerText);
+    displayInput.push(buttonValue);
+    //console.log(buttonValue);
   });
 });
 
 deleteBtn.addEventListener("click", () => {
   displayInput.pop();
-  console.log(displayInput);
+  // console.log(displayInput);
 });
 
 reset.addEventListener("click", () => {
   displayInput = [];
   console.log(displayInput);
+});
+
+equals.addEventListener("click", () => {
+  displayInput = eval(displayInput);
 });
